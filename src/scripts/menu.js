0015ae6efef7328927b89d64d236007f63a014b0
Elkin-test <elkin.perez@gopenux.com>
2025-07-15 16:43:05 -0500

    015 - Reestructuracion del proyecto - retomando

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,9 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
   document.querySelector('.nav-links').classList.toggle('expanded');
 });
+
+//Documentacion
+
+//https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_element_classlist_toggle
+
+
